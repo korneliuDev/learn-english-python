8b289d6e3beb0419bba145eda80bb52cfa776ae6
Kornel <89316637+korneliuDev@users.noreply.github.com>
2024-01-06 22:22:49 +0100

    Add files via upload

---
+++ angielskigra.py
@@ -0,0 +1,290 @@
+import time
+
+#nazwa gry & level 1
+print("")
+
+print("█░░ █▀▀ ▄▀█ █▀█ █▄░█   █▀▀ █▄░█ █▀▀ █░░ █ █▀ █░█   █▀█ █▄█ ▀█▀ █░█ █▀█ █▄░█")
+print("█▄▄ ██▄ █▀█ █▀▄ █░▀█   ██▄ █░▀█ █▄█ █▄▄ █ ▄█ █▀█   █▀▀ ░█░ ░█░ █▀█ █▄█ █░▀█")
+
+print("")
+print("Level 1 , szkoła!")
+print("")
+time.sleep(1)
+                                                                                                                                           
+
+a = input('Podaj jak jest po angielsku "długopis": ' )
+if a == "pen":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to pen. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+b = input('Podaj jak jest po angielsku "książka": ' )
+if b == "book":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to book. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+c = input('Podaj jak jest po angielsku "klasa": ' )
+if c == "class":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to class. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+d = input('Podaj jak jest po angielsku "uczyć się": ' )
+if d == "learn":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to learn. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+e = input('Podaj jak jest po angielsku "egzamin": ' )
+if e == "exam":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to exam. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+f = input('Podaj jak jest po angielsku "ocena": ' )
+if f == "grade":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to grade. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+g = input('Podaj jak jest po angielsku "zeszyt, notes": ' )
+if g == "notebook":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to notebook. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+h = input('Podaj jak jest po angielsku "ołówek": ' )
+if h == "pencil":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to pencil. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+i = input('Podaj jak jest po angielsku "szafka szkolna": ' )
+if i == "school box" or "locker":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to school box lub locker. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+j = input('Podaj jak jest po angielsku "zeszyt ćwiczeń": ' )
+if j == "workbook":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to workbook. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+time.sleep(3)
+
+level1 = input("Ukonczyłeś level 1! Czy chcesz przejść do następnego?: ")
+if level1 == "nie":
+    exit
+else:
+    print("Za 5 sekund przejdziesz dalej!")
+time.sleep(5)
+
+print("")
+print("Level 2 , kuchnia!")
+print("")
+time.sleep(1)
+
+a1 = input('Podaj jak jest po angielsku "kuchnia": ' )
+if a1 == "kitchen":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to kitchen. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+b1 = input('Podaj jak jest po angielsku "lodówka": ' )
+if b1 == "fridge":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to fridge. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+c1 = input('Podaj jak jest po angielsku "kuchenka": ' )
+if c1 == "cooker":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to cooker. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+d1 = input('Podaj jak jest po angielsku "mikrofalówka": ' )
+if d1 == "microwave":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to microwave. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+e1 = input('Podaj jak jest po angielsku "szafka": ' )
+if e1 == "cabinet":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to cabinet. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+f1 = input('Podaj jak jest po angielsku "zmywarka": ' )
+if f1 == "dishwasher":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to dishwasher. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+g1 = input('Podaj jak jest po angielsku "czajnik": ' )
+if g1 == "kettle":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to kettle. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+h1 = input('Podaj jak jest po angielsku "piekarnik": ' )
+if h1 == "oven":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to oven. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+i1 = input('Podaj jak jest po angielsku "garnek": ' )
+if i1 == "pot":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to pot. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+j1 = input('Podaj jak jest po angielsku "okap": ' )
+if j1 == "hood":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to hood. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+
+level2 = input("Ukonczyłeś level 2! Czy chcesz przejść do następnego?: ")
+if level2 == "nie":
+    exit
+else:
+    print("Za 5 sekund przejdziesz dalej!")
+time.sleep(5)
+
+print("")
+print("Level 3 , własny pokój!")
+print("")
+time.sleep(1)
+
+a3 = input('Podaj jak jest po angielsku "łóżko": ' ) #1
+if a3 == "bed":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to bed. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+b3 = input('Podaj jak jest po angielsku "biurko": ' ) #2
+if b3 == "desk":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to desk. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+c3 = input('Podaj jak jest po angielsku "krzesło": ' ) #3
+if c3 == "chair":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to chair. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+d3 = input('Podaj jak jest po angielsku "komoda": ' ) #4
+if d3 == "chest of drawers" or "commode":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to chest of drawers lub commode. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+e3 = input('Podaj jak jest po angielsku "półka": ' ) #5
+if e3 == "shelf":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to shelf. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+f3 = input('Podaj jak jest po angielsku "okno": ' ) #6
+if f3 == "window":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to window. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+g3 = input('Podaj jak jest po angielsku "dzwi": ' ) #7
+if g3 == "door":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to door. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+h3 = input('Podaj jak jest po angielsku "sypialnia": ' ) #8
+if h3 == "bedroom":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to bedroom. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+i3 = input('Podaj jak jest po angielsku "telewizor": ' ) #9
+if i3 == "tv" or "TV":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to TV. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+j3 = input('Podaj jak jest po angielsku "telewizor": ' ) #10
+if j3 == "tv" or "TV":
+    print("Poprawna odpowiedź!")
+else:
+    print("Zła odpowiedź, poprawna to TV. Spróbuj następnym razem!")
+time.sleep(1)
+print(" ")
+
+print("Uko") ,  time.sleep(1) , print("ńczy") , time.sleep(1) , print("łeś") , time.sleep(1)
+print("Pierwszą") , time.sleep(1)
+print("Wersję Tej Gry!") , time.sleep(1)
+
+print("Czekaj na następne wersje!")
+
+print("█░░ █▀▀ ▄▀█ █▀█ █▄░█   █▀▀ █▄░█ █▀▀ █░░ █ █▀ █░█   █▀█ █▄█ ▀█▀ █░█ █▀█ █▄░█")
+print("█▄▄ ██▄ █▀█ █▀▄ █░▀█   ██▄ █░▀█ █▄█ █▄▄ █ ▄█ █▀█   █▀▀ ░█░ ░█░ █▀█ █▄█ █░▀█")
+print("by korneliudev")
